fe26ef44ee55ad5b1f4017b931597da045f02b23
David Roulin <freelance@davidroulin.ch>
2013-04-20 21:55:34 +0200

    images of satellites + list of PDFs from 2012

---
+++ mobile_app/swissmocca_prototype1/config.lua
@@ -0,0 +1,22 @@
+application =
+{
+	content =
+	{
+		--[[
+		--width = 380,
+		--height = 570,
+		width = 320,
+		height = 480,
+		--scale = "zoomEven",
+		scale = "letterbox",
+		]]--
+		width = 768,
+		height = 1024,
+		scale = "letterbox",
+		xAlign = "center",
+		yAlign = "center",
+		imageSuffix = {
+			["@x2"] = 2
+		}
+	}
+}
---
+++ mobile_app/swissmocca_prototype1/docs/docs.lua
@@ -0,0 +1,7 @@
+docs = {
+	{ "326337main_Cubesat backgrounder.pdf", "pdf" },
+	{ "Eutelsat_bus_spec.pdf", "pdf" },
+	{ "FS003_12_ATV_updated_launch_2008.pdf", "pdf" },
+	{ "OnyxSatellite_SS.pdf", "pdf" },
+	{ "slave_microcontroller_spec.pdf", "pdf" },
+}
